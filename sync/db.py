4f5aca1c690fee3d3790ab1154ed4c7a362fbabc
itswcg <itswcg@gmail.com>
2018-10-11 13:48:02 +0800

    feat(db.py): sql

--- sync/db.py
+++ sync/db.py
@@ -4,11 +4,11 @@ from aiomysql.sa import create_engine
 
 metadata = sa.MetaData()
 
-msg = sa.Table('msg', metadata,
-               sa.Column('id', sa.Integer, primary_key=True),
-               sa.Column('fromUserId', sa.String(10)),
-               sa.Column('toUserId', sa.String(10)),
-               sa.Column('content', sa.String(1000)))
+# msg = sa.Table('msg', metadata,
+#                sa.Column('id', sa.Integer, primary_key=True),
+#                sa.Column('fromUserId', sa.String(10)),
+#                sa.Column('toUserId', sa.String(10)),
+#                sa.Column('content', sa.String(1000)))
 
 
 async def init_db(app):
@@ -27,6 +27,6 @@ async def close_db(app):
 
 async def create_msg(conn, fromUserId, toUserId, content):
     tx = await conn.begin()
-    ms = msg.insert().values(fromUserId=fromUserId, toUserId=toUserId, content=content)
-    await conn.execute(ms)
+    # ms = msg.insert().values(fromUserId=fromUserId, toUserId=toUserId, content=content)
+    await conn.execute("INSERT INTO msg (fromUserId, toUserId, content) VALUES (%s, %s, %s)", (fromUserId, toUserId, content))
     await tx.commit()
--- sync/test.py
+++ sync/test.py
@@ -2,9 +2,13 @@ import json
 
 import aiohttp
 import asyncio
+import requests
 
 payload = {'fromUserId': '1', 'toUserId': '2', 'content': 'hello, world'}
 headers = {'content-type': 'application/json'}
+
+header = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'}
 
 
 async def fetch(session, url):
@@ -12,13 +16,27 @@ async def fetch(session, url):
         return await response.text()
 
 
+async def getch(session, url):
+    async with session.get(url, headers=header) as response:
+        # return await response.text()
+        return
+
+
 async def main():
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://0.0.0.0:8080/')
+        # html = await fetch(session, 'http://0.0.0.0:8080/')
+        html = await getch(session, 'http://blog.itswcg.com')
         print(html)
+
+
+def test():
+    requests.get('http://blog.itswcg.com')
+    print('success')
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    tasks = [main() for i in range(5000)]
+    tasks = [main() for i in range(100)]
     loop.run_until_complete(asyncio.wait(tasks))
+    # for _ in range(100):
+    #     test()
